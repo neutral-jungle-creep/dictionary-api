9f25598a4b62a533711a1c613a392a4a74c9e799
neutral-jungle-creep <arrrseny11@gmail.com>
2022-11-15 00:16:25 +0300

    add func showWords to delivery\show.go

--- pkg/delivery/show.go
+++ pkg/delivery/show.go
@@ -2,9 +2,15 @@ package delivery
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 	"net/http"
 )
 
 func (h *Handler) showWords(c *gin.Context) {
-	c.JSON(http.StatusOK, "hello")
+	words, err := h.service.Show.ShowWords()
+	if err != nil {
+		logrus.Errorf("error in showWords func, %s", err.Error())
+		c.AbortWithStatusJSON(http.StatusInternalServerError, err.Error())
+	}
+	c.JSON(http.StatusOK, words)
 }
