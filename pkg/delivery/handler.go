7b6b6028e92eab30e292d7d0406eb3219ba36a4f
neutral-jungle-creep <arrrseny11@gmail.com>
2022-11-17 01:36:41 +0300

    move all funcs to handler.go

--- pkg/delivery/handler.go
+++ pkg/delivery/handler.go
@@ -2,7 +2,10 @@ package delivery
 
 import (
 	"dictionary/pkg/service"
+	"dictionary/pkg/service/dto"
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"net/http"
 )
 
 type Handler struct {
@@ -22,9 +25,42 @@ func (h Handler) InitRoutes() *gin.Engine {
 	{
 		show.GET("/all-words", h.showWords)
 	}
-	edit := router.Group("/write")
+	write := router.Group("/write")
 	{
-		edit.POST("/add-new-word", h.addNewWord)
+		write.POST("/add-new-word", h.addNewWord)
 	}
 	return router
+}
+
+func (h *Handler) showWords(c *gin.Context) {
+	words, err := h.service.Show.GetAllWords()
+	if err != nil {
+		NewException("showWords", c, http.StatusInternalServerError, err).ExceptionResp()
+		return
+	}
+	logrus.Infof("showWords completed successfully, %d", http.StatusOK)
+	c.JSON(http.StatusOK, words)
+}
+
+type UserRequestWord struct {
+	ForeignWord string `json:"word" binding:"required"`
+	Translation string `json:"translation" binding:"required"`
+}
+
+func (h *Handler) addNewWord(c *gin.Context) {
+	var input UserRequestWord
+
+	if err := c.BindJSON(&input); err != nil {
+		NewException("addNewWord", c, http.StatusBadRequest, err).ExceptionResp()
+		return
+	}
+
+	wordDto := dto.NewWordDto(0, input.ForeignWord, input.Translation)
+	if err := h.service.Edit.AddWord(wordDto); err != nil {
+		NewException("addNewWord", c, http.StatusInternalServerError, err).ExceptionResp()
+		return
+	}
+
+	logrus.Infof("addNewWord completed successfully, %d", http.StatusOK)
+	c.JSON(http.StatusOK, "слово успешно добавлено")
 }
