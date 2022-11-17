fef1816cfb8f53e1ffe644343f1ccdc02a1b8591
neutral-jungle-creep <arrrseny11@gmail.com>
2022-11-17 23:08:55 +0300

    add Learned bool to models structs

--- pkg/domain/word.go
+++ pkg/domain/word.go
@@ -4,12 +4,14 @@ type Word struct {
 	Id          int
 	ForeignWord string
 	Translation string
+	Learned     bool
 }
 
-func NewWord(id int, word string, translation string) *Word {
+func NewWord(id int, word string, translation string, learned bool) *Word {
 	return &Word{
 		Id:          id,
 		ForeignWord: word,
 		Translation: translation,
+		Learned:     learned,
 	}
 }
--- pkg/service/dto/word.go
+++ pkg/service/dto/word.go
@@ -4,12 +4,14 @@ type WordDto struct {
 	Id          int    `json:"id"`
 	ForeignWord string `json:"word"`
 	Translation string `json:"translation"`
+	Learned     bool   `json:"learned"`
 }
 
-func NewWordDto(id int, word string, translation string) *WordDto {
+func NewWordDto(id int, word string, translation string, learned bool) *WordDto {
 	return &WordDto{
 		Id:          id,
 		ForeignWord: word,
 		Translation: translation,
+		Learned:     learned,
 	}
 }
