a55a6616a5700f7e6d7d2c2c4f7a1d7d72f7e2af
neutral-jungle-creep <arrrseny11@gmail.com>
2022-11-19 20:32:40 +0300

    fix word

--- pkg/service/dto/word.go
+++ pkg/service/dto/word.go
@@ -3,7 +3,7 @@ package dto
 type WordDto struct {
 	Id          int    `json:"id"`
 	ForeignWord string `json:"word"`
-	Translation string `json:"translation"`
+	Translation string `json:"transl"`
 	Learned     bool   `json:"learned"`
 }
 
